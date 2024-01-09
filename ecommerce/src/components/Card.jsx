da6c19b4a828dd97891d2e5443651d7df14571a6
FacuHdezz <facuhernandez2000@gmail.com>
2024-01-09 00:49:44 -0300

    mejorando el bento grid

--- ecommerce/src/components/Card.jsx
+++ ecommerce/src/components/Card.jsx
@@ -1,10 +1,9 @@
 import { useEffect, useState } from "react";
 
-const Card = ({img, description, id}) => {
+const Card = ({ img, description, id }) => {
 
     return (
         <div id={id}>
-            <h1>{description}</h1>
             <img src={img} alt={description} />
         </div>
     )
